261fa650d84442ff497323c6e54b3dafdc22c17b
DannyEzechukwu <dezechuk@gmail.com>
2023-11-25 12:19:54 -0600

    Adjusted data model to be more concise. Updated model.py to reflect new data model. Included crud functions to create users, events, and scheduled events

---
+++ crud.py
@@ -0,0 +1,33 @@
+from model import User, ScheduledEvent, Event, 
+
+#User crud functions
+
+def create_user(fname, lname, username, email, password):
+    
+    user = User(fname = fname, 
+            lname = lname, 
+            username = username, 
+            email = email,
+            password = password)
+    
+    return user
+
+#Event crud functions
+def schedule_event(user_id, event_id, weekday, month, date, event_start_time, event_end_time): 
+    
+    scheduled_event = ScheduledEvent(user_id = user_id, 
+            event_id = event_id, 
+            weekday = weekday,
+            month = month,
+            date = date,
+            event_start_time = event_start_time, 
+            event_end_time = event_end_time)
+    
+    return scheduled_event
+
+#Day crud functions
+def create_event(name, description): 
+    day = Event(name = name, 
+            description = description)
+    
+    return day
--- model.py
+++ model.py
@@ -47,16 +47,16 @@ class User(db.Model):
                     nullable = False)
     
     #User can schedule many events
-    events = db.relationship("Event", back_populates = "user")
+    events = db.relationship("ScheduledEvent", back_populates = "user")
     
     def __repr__(self): 
         return f"<User{self.id} {self.username} {self.email}"
     
 
-#Event Table
-class Event(db.Model): 
+#ScheduledEvent Table
+class ScheduledEvent(db.Model): 
     
-    __tablename__ = "events"
+    __tablename__ = "scheduled_events"
 
     id = db.Column(db.Integer, 
                 primary_key = True,
@@ -67,117 +67,64 @@ class Event(db.Model):
                         nullable = False, 
                         index = True)
     
-    event_name = db.Column(db.String, 
-                           nullable = False,
-                           index = True)
+    event_id = db.Column(db.Integer, 
+                        db.ForeignKey("events.id"),
+                        index = True)
+
+    weekday = db.Column(db.String, 
+                index = True, 
+                nullable = False)
     
-    event_description = db.Column(db.String, 
-                            nullable = False,
-                            index = True)
+    month = db.Column(db.String, 
+                    index = True, 
+                    nullable = False)
     
-    #One event can belong to only one one user
+    date = db.Column(db.Integer, 
+                    index = True, 
+                    nullable = False)
+    
+    event_start_time = db.Column(db.String,
+                        nullable = False,
+                        index = True)
+    
+    event_end_time = db.Column(db.String,
+                        nullable = False,
+                        index = True)
+    
+    #1 scheduled event can be scheduled by 1 user
     user = db.relationship("User", back_populates = "events")
 
-    #One event can belong to only one day
-    day = db.relationship("Day", back_populates = "events")
-
-    #One event can only be held at one time
-    time = db.relationship("Time", back_populates="event", uselist=False)
-    
+    #1 scheuled event can contain 1 event
+    event = db.relationship("Day", back_populates = "events")
     
     def __repr__(self): 
-        return f"<Event{self.id} {self.event_name} - {self.event_description}>"
-    
+        return f"<Event{self.event_id} - {self.weekday}- {self.event_description}>"
 
-#Month Table
-class Month(db.Model): 
-    __tablename__ = "months"
+
+#Event Table
+class Event(db.Model): 
+    __tablename__ = "events"
 
     id = db.Column(db.Integer, 
                 primary_key = True,
                 index = True)
     
-    month_name = db.Column(db.String, 
+    name = db.Column(db.String,
                         nullable = False, 
                         index = True)
     
-    #One month can have several days
-    days = db.relationship("Day", back_populates = "month")
-    
-    def __repr__(self): 
-        return f"<Month{self.id} - {self.month_name}>"
-    
-
-#Day Table
-class Day(db.Model): 
-    __tablename__ = "days"
-
-    id = db.Column(db.Integer, 
-                primary_key = True,
-                index = True)
-    
-    month_id = db.Column(db.Integer,
-                        db.ForeignKey("months.id"),
+    description = db.Column(db.String,
+                        nullable = False,
                         index = True)
     
-    day_name = db.Column(db.String,
-                        nullable = False, 
-                        index = True)
-    
-    day_number= db.Column(db.Integer,
-                        nullable = False, 
-                        index = True)
-    
-    event_id = db.Column(db.Integer,
-                        db.ForeignKey("events.id"), 
-                        index = True)
-    
-    #One day can belong to only one month
-    month = db.relationship("Month", back_populates = "days") 
 
-    #One day can contain many events
-    events = db.relationship("Event", back_populates = "day")
-
-    #One day can contain many times 
-    times = db.relationship("Time", back_populates = "day" )
+    #1 type of event can be schedule ,many times
+    events = db.relationship("ScheduledEvent", back_populates = "event")
 
     def __repr__(self): 
-        return f"<{self.month_id}/{self.day_number} - {self.event_id}>"
+        return f"<Event{self.id} - {self.name}, {self.description}>"
 
-    
-#Time Table
-class Time(db.Model): 
-    __tablename__ = "times"
-
-    id = db.Column(db.Integer,
-                   primary_key = True, 
-                   index = True)
-    
-    day_id = db.Column(db.Integer, 
-                    db.ForeignKey("days.id"), 
-                    index = True)
-    
-    event_id = db.Column(db.Integer, 
-                    db.ForeignKey("events.id"), 
-                    index = True)
-    
-    start_time  = db.Column(db.String, 
-                        index = True)
-    
-    end_time = db.Column(db.String, 
-                        index = True)
-    
-    #A single time could only belong to one day
-    day = db.relationship("Day", back_populates = "times")
-
-    #A single time belongs to one event
-    event = db.relationship("Event", back_populates="time")
-    
-    def __repr__(self): 
-        return f"<Event{self.event_id}: {self.start_time} - {self.end_time}>"
-
-
-    
+   
 if __name__ == "__main__":
     from server import app
 
